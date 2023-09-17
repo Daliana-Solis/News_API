9f6373db41652a8173a37314e33b65b1f2671848
Daliana-Solis <dsolisso@uci.edu>
2023-09-17 18:30:44 -0500

    Removed personal email

--- send_email.py
+++ send_email.py
@@ -5,10 +5,12 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "daliana.solis29@gmail.com"
+    #username = gmail
+    username = "X"
     password = "gvpn jxut ilap nsye"
 
-    receiver = "daliana.solis29@gmail.com"
+    #receiver = gmail
+    receiver = "X"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
